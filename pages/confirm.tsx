4d591b3c54e9ceb9559bb76eedcc43eec68f6523
Paul Pessoa <paulmspessoa@gmail.com>
2024-04-11 20:47:01 -0300

    create confirm page

---
+++ pages/confirm.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import { useRouter } from 'next/router';
+
+const ConfirmationPage = () => {
+    const router = useRouter();
+    const handleRedirectToHome = () => {
+        router.push('/');
+    };
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh',
+            }}
+        >
+            <Typography fontSize={18} align="center" color="white" mb={2}>
+                Tu orden ha sido confirmada.
+            </Typography>
+            <Button sx={{
+                color: "#000000",
+                backgroundColor: "#FACE39",
+                "&:hover": {
+                    backgroundColor: "#d6a400"
+                },
+            }} variant="contained" onClick={handleRedirectToHome}>
+                Volver al inicio
+            </Button>
+        </Box>
+    );
+};
+
+export default ConfirmationPage;
