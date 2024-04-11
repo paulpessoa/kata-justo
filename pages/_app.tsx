46ae754c9c03602ab45d64360d64e3e1b306c27f
Paul Pessoa <paulmspessoa@gmail.com>
2024-04-11 20:39:38 -0300

    adjust color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { AppProps } from "next/app";
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Box>
+    <Box sx={{backgroundColor: "#212121", display: "flex", flexDirection:"column", alignContent: "center", alignItems: "center", minHeight: "100vh"}}>
       <Component {...pageProps} />
     </Box>
   );
